c1bc6c7535b02f18c1738a591f95a307892056aa
INationalDay <1903601292@qq.com>
2018-10-31 00:28:32 +0800

    update

--- miniApp.py
+++ miniApp.py
@@ -3,9 +3,16 @@
 Created by zhaoguoqing on 18/10/30
 """
 from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+import pymongo
 import click
 
 app = Flask(__name__)
+# app.config['MONGO_URI'] = 'mongodb://localhost:27017/zgq'
+# mongo = PyMongo(app)
+
+mongo = pymongo.MongoClient('mongodb://localhost:27017/zgq')
+
 
 @app.route('/')
 def index():
@@ -14,6 +21,12 @@ def index():
 @app.route('/json')
 def json():
     return jsonify({'name': 'bob', 'age': 13})
+
+@app.route('/book')
+def book():
+    books = [i for i in mongo['zgq']['douban_book'].find({}, {'_id': 0}).limit(10)]
+    # print(books)
+    return jsonify(books)
 
 @app.cli.command()
 def cmd_test():
