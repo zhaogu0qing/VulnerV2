1482dc05078a75b13e3a33e64417875ba8799476
INationalDay <1903601292@qq.com>
2018-10-30 23:51:15 +0800

    del useless

--- app/templates/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time    : 2018/10/30 19:36
-@Author  : zhaoguoqing600689
-@File    : __init__.py.py
-@Software: PyCharm
-"""
--- config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time    : 2018/10/30 19:40
-@Author  : zhaoguoqing600689
-@File    : config.py
-@Software: PyCharm
-"""
